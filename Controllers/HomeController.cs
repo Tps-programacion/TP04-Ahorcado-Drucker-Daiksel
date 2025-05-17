118b5cfb60d0bfa3141d0261fa0d33c4539aac6d
Federico Drucker <fededr81@gmail.com>
2025-05-16 21:11:34 -0300

    Logica base hecha, falta frontend y un par de soluciones de errores (si es que hay)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace TP04_Drcuker_Daiksel.Controllers;
 
 public class HomeController : Controller
 {
+    
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
@@ -22,31 +23,57 @@ public class HomeController : Controller
     public IActionResult jugar(){
         
         ViewBag.palabraAdivinar = Ahorcado.letrasAdivinadas;
-        ViewBag.utilizadas = Ahorcado.fallos;
+        ViewBag.fallos = Ahorcado.fallos;
         ViewBag.intentos = Ahorcado.intentos;
         ViewBag.letrasFalladas = Ahorcado.letrasFalladas;
         ViewBag.perdio = Ahorcado.perdio;
         ViewBag.gano = Ahorcado.gano;
+        ViewBag.cantidadLetras = Ahorcado.letras.Length;
+        
+        ViewBag.mensaje = "Aca se van a mostrar los mensajes de consola";
         return View("juego");
     }
 
-    public IActionResult verificar(char letraAdivinada, string palabraAdivinada){
+    public IActionResult verificar(char letraAdivinada, string palabraAdivinada)
+    {
+
         string devuelta = "";
-        if(Ahorcado.finalizar == false){
-            if(palabraAdivinada == null){
+        if (Ahorcado.finalizar == false)
+        {
+            if (palabraAdivinada == null)
+            {
                 devuelta = Ahorcado.adivinarLetra(letraAdivinada);
                 ViewBag.mensaje = devuelta;
-                return View("juego");
             }
-            else{
+            else
+            {
                 devuelta = Ahorcado.arriesgarPalabraCompleta(palabraAdivinada);
                 ViewBag.mensaje = devuelta;
-                return View("juego");
             }
         }
-        else{
-            
+
+        ViewBag.palabraAdivinar = Ahorcado.letrasAdivinadas;
+        ViewBag.fallos = Ahorcado.fallos;
+        ViewBag.intentos = Ahorcado.intentos;
+        ViewBag.letrasFalladas = Ahorcado.letrasFalladas;
+        ViewBag.perdio = Ahorcado.perdio;
+        ViewBag.gano = Ahorcado.gano;
+        ViewBag.cantidadLetras = Ahorcado.letras.Length;
+
+        if (Ahorcado.finalizar == false)
+        {
+            return View("Juego");
         }
-        
+        else
+        {
+            if (Ahorcado.gano == true)
+            {
+                return View("vistaGanador");
+            }
+            else
+            {
+                return View("vistaPerdedor");
+            }
+        }
     }
 }
--- Models/Ahorcado.cs
+++ Models/Ahorcado.cs
@@ -32,27 +32,28 @@ public static class Ahorcado
     public static string adivinarLetra(char letra)
     {
         string devolver;
+        char letraEntrada = char.ToUpper(letra);
         intentos++;
 
-        if (letrasFalladas.Contains(letra) || letrasAdivinadas.Contains(letra))
+        if (letrasFalladas.Contains(letraEntrada) || letrasAdivinadas.Contains(letraEntrada))
         {
             devolver = "Ya intentaste con esta letra";
         }
-        else if (letras.Contains(letra))
+        else if (letras.Contains(letraEntrada))
         {
             for (int i = 0; i < letras.Length; i++)
             {
-                if (letras[i] == letra)
-                {
-                    letrasAdivinadas[i] = letra;
-                }
+            if (letras[i] == letraEntrada)
+            {
+                letrasAdivinadas[i] = letraEntrada;
+            }
             }
             devolver = "Adivinaste!";
         }
         else
         {
             fallos++;
-            letrasFalladas.Add(letra);
+            letrasFalladas.Add(letraEntrada);
             devolver = "Incorrecto!";
         }
 
@@ -70,23 +71,26 @@ public static class Ahorcado
         }
 
         return devolver;
-    }
+        }
 
 
-    static public string arriesgarPalabraCompleta(string palabra){
-        
-        string devolver = "";
-        finalizar = true;
-        if(palabra == PALABRA){
+        static public string arriesgarPalabraCompleta(string palabra)
+        {
+            string devolver = "";
+            palabra = palabra.ToUpper(); 
+            finalizar = true;
+            if (palabra == PALABRA)
+            {
             gano = true;
             devolver = "Le pegaste, crack";
-        }
-        else{
+            }
+            else
+            {
             devolver = "No le pegaste, panflin";
             perdio = true;
-        }
+            }
 
-        return devolver;
-    }
+            return devolver;
+        }
 
 }
